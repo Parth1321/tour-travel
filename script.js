8d9b2eb4706786760fdaff13c52c1d75e592b9a8
Parth <140968140+Parth1321@users.noreply.github.com>
2024-12-02 21:23:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// gsap.from("#page1 #card-content1",{
+//     scale:0,
+//     delay:1,
+//     scrollTrigger:{
+//         trigger:"#page1 #card-content1",
+//         scroller: "body",
+//         markers: true,
+//         start: "top 80vh",
+//         scrub: 3,
+//     }
+// })
+
+// gsap.from("#page1 i",{
+//     transfrom:"translateX(-150%)",
+//     scrollTrigger:{
+//         trigger:"page1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 80%",
+//         end:"top -100%",
+//         scrub:2,
+//         // pin:true
+//     }
+// })
